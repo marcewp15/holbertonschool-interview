5a6b78bfb0d1059c7a84be20c4a2b6d9cd6e9ba6
marcewp15 <970@holbertonschool.com>
2020-07-21 18:25:58 -0500

    This  method determines if all the boxes can be opened

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+def canUnlockAll(boxes):
+
+    keys = [0]
+
+    for x in keys:
+        for y in boxes[x]:
+            if y < len(boxes) and y not in keys:
+                keys.append(y)
+    if len(boxes) == len(keys):
+        return True
+    return False
