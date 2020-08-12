c4e970ed24247e27ee24f3b90c6072191d56ae0a
marcewp15 <970@holbertonschool.com>
2020-08-11 20:31:52 -0500

    Testing code

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+def minOperations(n):
+    copy = 1
+    count = 0
+    minimo = []
+
+    for paste in range(1, n):
+        for number in range(n):
+            copy += copy * paste
+            count += paste + 1
+
+            if n <= copy:
+                minimo.append(count)
+                break
+        count = 0
+        copy = 1
+    return(min(minimo))
