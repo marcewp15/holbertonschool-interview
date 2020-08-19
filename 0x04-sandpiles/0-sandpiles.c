954e4a91fc0dec3ee9d8248ee28f0dac406a194f
marcewp15 <970@holbertonschool.com>
2020-08-18 23:49:34 -0500

    computes the sum of two sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -45,33 +45,47 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		{
 			printf("=\n");
 			print_grid(grid1);
-			for (x = 0; x < 3; x++)
-			{
-				for (y = 0; y < 3; y++)
-				{
-					if (grid1[x][y] >= 4)
-					{
-						grid1[x][y] = grid1[x][y] - 4;
-						if ((x + 1 >= 0) && (x + 1 < 3))
-							grid2[x + 1][y]++;
-						if ((x + 1 >= 0) && (x - 1 < 3))
-							grid2[x - 1][y]++;
-						if ((y + 1 >= 0) && (y + 1 < 3))
-							grid2[x][y + 1]++;
-						if ((y - 1 >= 0) && (y - 1 < 3))
-							grid2[x][y - 1]++;
-					}
-				}
-			}
+			path_grid(grid1, grid2);
 		}
 		else
 			break;
 	}
 }
 /**
+ * path_grid - make the sanpiles stable
+ * @grid1: bidimentional array x
+ * @grid2: bidimentional array y
  *
+ * Return: void statement
+ **/
+void path_grid(int grid1[3][3], int grid2[3][3])
+{
+	int x, y;
+
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			if (grid1[x][y] >= 4)
+			{
+				grid1[x][y] = grid1[x][y] - 4;
+				if ((x + 1 >= 0) && (x + 1 < 3))
+					grid2[x + 1][y]++;
+				if ((x + 1 >= 0) && (x - 1 < 3))
+					grid2[x - 1][y]++;
+				if ((y + 1 >= 0) && (y + 1 < 3))
+					grid2[x][y + 1]++;
+				if ((y - 1 >= 0) && (y - 1 < 3))
+					grid2[x][y - 1]++;
+			}
+		}
+	}
+}
+/**
+ * valid - make the sanpiles stable
+ * @grid: bidimentional array
  *
- *
+ * Return: 1 if is higher than 4 or 0 if not
  **/
 int valid(int grid[3][3])
 {
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,5 +5,6 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void path_grid(int grid1[3][3], int grid2[3][3]);
 int valid(int grid[3][3]);
 #endif
