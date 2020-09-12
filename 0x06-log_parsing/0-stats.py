4a671ff7c25419b3f6d395e3934e33deccc71d1a
marcewp15 <970@holbertonschool.com>
2020-09-12 12:47:21 -0500

    This script reads stdin line by line and computes metrics (log parsing)

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
         od = collections.OrderedDict(sorted(status.items()))
         for keys, values in od.items():
             if values != 0:
-                print('{} : {}'.format(keys, values))
+                print('{}: {}'.format(keys, values))
         sys.exit()
