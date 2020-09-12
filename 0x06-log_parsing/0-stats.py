4c8f09ad2cd36d5bd8ecd8278c86796a1905e214
marcewp15 <970@holbertonschool.com>
2020-09-12 12:45:17 -0500

    This script reads stdin line by line and computes metrics (log parsing)

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
 """ reads stdin line by line and computes metrics """
 
-import sys
-import collections
+if __name__ == '__main__':
 
-cont = 0
-size = 0
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
+    import sys
+    import collections
 
-try:
-    for line in sys.stdin:
-        cont += 1
-        fs = line.split()
-        size += int(fs[-1])
+    cont = 0
+    size = 0
+    status = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
 
-        for keys in status:
-            if keys == fs[-2]:
-                status[keys] += 1
+    try:
+        for line in sys.stdin:
+            cont += 1
+            fs = line.split()
+            size += int(fs[-1])
 
-        if cont == 10:
-            print('File size: {}'.format(size))
-            od = collections.OrderedDict(sorted(status.items()))
-            for keys, values in od.items():
-                if values != 0:
-                    print('{}: {}'.format(keys, values))
-            cont = 0
+            for keys in status:
+                if keys == fs[-2]:
+                    status[keys] += 1
 
-except KeyboardInterrupt:
-    print('File size: {}'.format(size))
-    od = collections.OrderedDict(sorted(status.items()))
-    for keys, values in od.items():
-        if values != 0:
-            print('{} : {}'.format(keys, values))
-    sys.exit()
+            if cont == 10:
+                print('File size: {}'.format(size))
+                od = collections.OrderedDict(sorted(status.items()))
+                for keys, values in od.items():
+                    if values != 0:
+                        print('{}: {}'.format(keys, values))
+                cont = 0
+
+    except KeyboardInterrupt:
+        print('File size: {}'.format(size))
+        od = collections.OrderedDict(sorted(status.items()))
+        for keys, values in od.items():
+            if values != 0:
+                print('{} : {}'.format(keys, values))
+        sys.exit()
